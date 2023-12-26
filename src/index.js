002ddf9c2ec4d806eb47f0dc7916351d40ac8b8b
xxl <3578144921@qq.com>
2023-12-26 11:33:30 +0800

    加图标

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import App from "./container/App";
 import ReactDOM from 'react-dom/client';
-import './index.css'
-import './tailwind.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 // 渲染
