ff66e7b00cdb9f681f065d22645dd00dd20524a1
xxl <3578144921@qq.com>
2024-04-19 03:58:14 +0800

    脚手架重构完成

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,17 +1,12 @@
 import {lazy} from "react";
 
 const Home = lazy(() => import('/src/views/home/Home'))
-const Test = lazy(() => import('/src/views/test/Test'))
 
 export default function routes() {
     return [
         {
             path: "/",
             element: <Home/>
-        },
-        {
-            path: "/test",
-            element: <Test/>
         }
     ];
 }
