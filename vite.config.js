93961dd14f900f0df8c895b6f456b4515f9409e9
xxl <3578144921@qq.com>
2024-06-23 01:14:40 +0800

    feat: 增加网络配置

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,18 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      UnoCSS()
-  ],
-  resolve:{
-      alias:{
-          '@':resolve('src')
-      }
-    }
+    plugins: [
+        react(),
+        UnoCSS()
+    ],
+    resolve:{
+        alias:{
+            '@':resolve('src')
+        }
+    },
+    server: {
+        host: '0.0.0.0', // 监听所有网络接口
+        port: 5173, // 你可以指定一个端口
+    },
 })
 
