4dc84373b48b031f59b1ac912e4a445a4934cf93
xxl <3578144921@qq.com>
2024-07-29 14:12:29 +0800

    fix: 修改路由导出

--- src/hook/useFetch.jsx
+++ src/hook/useFetch.jsx
@@ -4,23 +4,25 @@ import {debounce} from "@/lib/toolkit/debounce.js";
 
 /**
  * 用于请求的HOOK
- * @param defaultValue 请求的默认值
  * @param requestMethod 请求方法
+ * @param initResp 初始响应
  * @param debounceTime 接口防抖时间
  * @returns response 请求返回值
  * @returns error 请求的错误
  * @returns setProxyMethodParam  设置代理请求方法的参数
  */
-const useFetch = (defaultValue,requestMethod,debounceTime = 300) =>{
+const useFetch = (requestMethod,initResp = null,debounceTime = 300) =>{
     //请求返回值
-    const [response, setResponse] = useState(defaultValue)
+    const [response, setResponse] = useState(initResp)
     //代理方法的请求参数
     const [requestMethodParam, setRequestMethodParam] = useState([])
-    //方法抛出异常的
+    //捕获异常
     const [error, setError] = useState(null)
+    //是否在加载
+    const [loading, setLoading] = useState(false)
 
     // 防抖的设置代理请求方法的参数
-    const debouncedSetRequestMethodParam = useCallback(debounce(setRequestMethodParam, debounceTime), [debounceTime]);
+    const debouncedSetRequestMethodParam = useCallback(debounce(setRequestMethodParam, debounceTime), []);
 
     //如果传NULL表示代理的方法不需要参数
     const setProxyMethodParam = (...param) => {
@@ -29,12 +31,14 @@ const useFetch = (defaultValue,requestMethod,debounceTime = 300) =>{
 
     useEffect(() => {
         try {
+            setLoading(true)
             //避免初次渲染出现
             if (!isNullOrUndefined(requestMethodParam) && requestMethodParam.length === 0) {
-              return
+                return
             }
             (async () => {
                 const resp = isNullOrUndefined(requestMethodParam) ? await requestMethod() : await requestMethod(...requestMethodParam);
+                setLoading(false)
                 setResponse(resp)
             })()
         } catch (e) {
@@ -45,7 +49,8 @@ const useFetch = (defaultValue,requestMethod,debounceTime = 300) =>{
     return [
         response,
         setProxyMethodParam,
-        error
+        error,
+        loading
     ]
 }
 
