a3801d982949454e18dd502ccde56f38ff016930
xxl <3578144921@qq.com>
2024-09-14 10:02:16 +0800

    fix(useFeatch): 修改请求HOOK

--- src/hook/useFetch.jsx
+++ src/hook/useFetch.jsx
@@ -1,9 +1,9 @@
-import {useCallback, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {isNullOrUndefined} from "@/lib/toolkit/util.js";
 import {debounce} from "@/lib/toolkit/debounce.js";
 
 /**
- * 用于请求的HOOK
+ * 请求代理HOOK，运用AOP
  * @param requestMethod 请求方法
  * @param initResp 初始响应
  * @param debounceTime 接口防抖时间
@@ -15,28 +15,27 @@ const useFetch = (requestMethod,initResp = null,debounceTime = 300) =>{
     //请求返回值
     const [response, setResponse] = useState(initResp)
     //代理方法的请求参数
-    const [requestMethodParam, setRequestMethodParam] = useState([])
+    const [requestMethodParam, setRequestMethodParam] = useState(null)
     //捕获异常
     const [error, setError] = useState(null)
-    //是否在加载
+    //是否在请求中
     const [loading, setLoading] = useState(false)
-    // 防抖的设置代理请求方法的参数
-    const debouncedSetRequestMethodParam = useCallback(debounce(setRequestMethodParam, debounceTime), []);
-
-    //如果传NULL表示代理的方法不需要参数
+    // 接口请求设置防抖
+    const debouncedSetRequestMethodParam = debounce(setRequestMethodParam, debounceTime)
+    //如果为NULL表示代理的方法不需要参数
     const setProxyMethodParam = (...param) => {
-        debouncedSetRequestMethodParam(param.length === 0 ? null : [...param]);
+        debouncedSetRequestMethodParam(param.length === 0 ? [] : [...param]);
     }
 
     useEffect(() => {
         try {
-            setLoading(true)
-            //避免初次渲染出现
-            if (!isNullOrUndefined(requestMethodParam) && requestMethodParam.length === 0) {
+            //避免初次渲染出现发送请求
+            if (isNullOrUndefined(requestMethodParam)) {
                 return
             }
             (async () => {
-                const resp = isNullOrUndefined(requestMethodParam) ? await requestMethod() : await requestMethod(...requestMethodParam);
+                setLoading(true)
+                const resp = requestMethodParam.length === 0 ? await requestMethod() : await requestMethod(...requestMethodParam);
                 setLoading(false)
                 setResponse(resp)
             })()
